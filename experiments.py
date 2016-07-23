468202f1c651781e21c93ffce7d39ae3c4b8bf61
Chaitanya <chait.m@monash.edu>
2016-07-23 13:01:41 +1000

    bug: The fields Kappa Temporal Statistic (percent), Kappa Statistic (percent) appear only intermittently in the output. Kappa M Statistic seemd to never appear
    This seems to be a MOA related bug.
    
    It would appear pandas is dropping columns... because there's nothing in some of the kappa statistic values it's getting from MOA- it's getting question marks. (And sometimes -Infinity)

--- experiments.py
+++ experiments.py
@@ -20,111 +20,115 @@ MOA_DIR = '/home/chait/moa-release-2016.04/'
 
 processes=[]
 
-#Decision Stump... used for testing script.
-
-moa_stump = "java -cp commons-math3-3.6.1.jar:moa.jar:cdgen.jar -javaagent:sizeofag-1.0.0.jar"
-moa_task = "moa.DoTask EvaluatePeriodicHeldOutTest"
-learner = "trees.DecisionStump"
-generator = "generators.WaveformGenerator"
-
 num_test_examples = 200 
 num_instances = 3000 
 test_interval = 100
 
+moa_stump = "java -cp commons-math3-3.6.1.jar:moa.jar:cdgen.jar -javaagent:sizeofag-1.0.0.jar"
+moa_task = "moa.DoTask EvaluateInterleavedTestThenTrain"
+learner = "-l moa.classifiers,bayes.NaiveBayes"
+generator = "-s \"\"\"(generators.categorical.AbruptDriftGenerator -b 1000)\"\"\""
+
+
 num_rows = num_instances/test_interval
 
-
-task_options = "\"\"\"" + " -l " + learner + " -s " + generator + " -n " + str(num_test_examples) + " -i " + str(num_instances) + " -f " + str(test_interval) + "\"\"\""
-
-cmd_seq = (moa_stump, moa_task, task_options)
-
-command = " ".join(cmd_seq)
-
-decision_stump_dir = "decision_stump"
+cmd_seq = [moa_stump, moa_task, learner, generator, "-i {i_val} -f {f_val} -q {q_val}".format(i_val = num_instances, f_val=test_interval, q_val=num_test_examples)]
+cmd = " ".join(cmd_seq) 
 
 number_of_streams = 10
 
 def main():
 
-  #run_experiment("trees.DecisionStump", "generators.WaveformGenerator", 10, 100, 25, 10, "dec_stump", "res")
-  experiment_average_over_streams(10, moa_stump + " " + "moa.DoTask EvaluateInterleavedTestThenTrain -l moa.classifiers.bayes.NaiveBayes -s \"\"\"(generators.categorical.AbruptDriftGenerator -b 1000)\"\"\" -i {i_val} -f {f_val} -q {q_val}".format(i_val = num_instances, f_val=test_interval, q_val=num_test_examples), "naive_bayes", "out")
+  cmd = moa_stump + " " + "moa.DoTask EvaluateInterleavedTestThenTrain -l moa.classifiers.bayes.NaiveBayes -s \"\"\"(generators.categorical.AbruptDriftGenerator -b 1000)\"\"\" -i {i_val} -f {f_val} -q {q_val}".format(i_val = num_instances, f_val=test_interval, q_val=num_test_examples)
+  mse = MultiStreamExperiment()
+  mse.average_over_streams(number_of_streams , cmd, "naive_bayes", "out")
 
   return 0
 
-def run_experiment(learner, generator, num_test_examples, num_instances, test_interval, number_of_streams, output_dir, file_prefix):
+# Composite of many instances of a given experiment running in parallel. Note that the seed for the random generator must change!
+class MultiStreamExperiment:
 
-  os.chdir(MOA_DIR)
-  utilities.remove_folder(output_dir)
-  utilities.make_folder(output_dir)
+  # Multiple stream Processes for this experiment
+  # https://docs.python.org/3/tutorial/classes.html
+  # Note that this is the way to create instance-specific variables- in init.
+  # Anything outside would be shared across all instances.
 
-  num_rows = num_instances/test_interval
+  def __init__(self):
+    self.processes = []  
 
-  task_options = "\"\"\"" + " -l " + learner + " -s " + generator + " -n " + str(num_test_examples) + " -i " + str(num_instances) + " -f " + str(test_interval) + "\"\"\""
-  cmd_seq = (moa_stump, moa_task, task_options)
-  command = " ".join(cmd_seq)
-
+  # The MOA processes created by the Experiment.run() method write to output files. Average over these.
+  def average_over_streams(self, num_streams, command_line, output_folder, file_prefix):
+    output_files = []
   
-  experiment_average_over_streams(number_of_streams, command, output_dir, file_prefix)
-
-  return
-
-def experiment_average_over_streams(num_streams, command_line, output_folder, file_prefix):
-  output_files = []
-
-  os.chdir(MOA_DIR)
-  utilities.remove_folder(output_folder)
-  utilities.make_folder(output_folder)
-  num_rows = num_instances/test_interval
-
-  folder_file_prefix = output_folder + "/" + file_prefix
-  #folder_file_prefix =  file_prefix
- 
-  for stream_num in range(0, num_streams):
-    output_files.append(folder_file_prefix + str(stream_num) + '.csv')
-  for stream_num in range(0, num_streams):
-    print output_files[stream_num]
-  for stream_num in range(0, num_streams):
-    run_single_experiment(command_line, output_files[stream_num])
-
-  exit_codes = [p.wait() for p in processes]
-  # wait until all experiments have finished running.
-
-  print output_files
-
-  dataframes = []
-  for file_name in output_files:
-    print file_name
-    dataframes.append(pd.read_csv(file_name, index_col=False, header=1, skiprows=0))
-    #dataframes.append(pd.read_csv(file_name, index_col=False, header=0))
-    # index_col has to be False as col 0 isn't integer
-
-  all_stream_learning_data = pd.concat(dataframes)
-  #all_stream_learning_data.to_csv("Cumulative.csv")
-
-  all_stream_mean = {}
-  for i in range(num_rows): # 
-    all_stream_mean[i] = all_stream_learning_data[i::num_rows].mean()
-    #print all_stream_mean[i]
-  all_stream_mean_df = pd.DataFrame(all_stream_mean)
-  all_stream_mean_df.to_csv(folder_file_prefix + "Mean.csv")
-    #print result_df
- 
-
-# Take a command line and create a process
-def run_single_experiment(command_line, output_file):
-
-  args = shlex.split(command_line)
-  #file_handle = os.path.join(MOA_DIR, output_file)
-  file_handle = output_file
-  print(args)
-  print(file_handle) 
-
+    os.chdir(MOA_DIR)
+    utilities.remove_folder(output_folder)
+    utilities.make_folder(output_folder)
+    num_rows = num_instances/test_interval
   
-  # create process
-  os.chdir(MOA_DIR)
-  out1 = open(file_handle, "w+")
+    folder_file_prefix = output_folder + "/" + file_prefix
+    # folder_file_prefix =  file_prefix
+   
+    # Create file names
+    for stream_num in range(0, num_streams):
+      output_files.append(folder_file_prefix + str(stream_num) + '.csv')
+    # Print them
+    for stream_num in range(0, num_streams):
+      print output_files[stream_num]
+    # Run experiments setting output files
+    for stream_num in range(0, num_streams):
+      e = Experiment()
+      e.run(command_line ,output_files[stream_num], self.processes)
+  
+    exit_codes = [p.wait() for p in self.processes]
+    # wait until all experiments have finished running: each experiment corresponds to a process.
+  
+    print output_files
+  
+    # Load the outputs into dataframes to prepare for averaging
+  
+    dataframes = []
+    for file_name in output_files:
+      print file_name
+      dataframes.append(pd.read_csv(file_name, index_col=False, header=1, skiprows=0))
+      # index_col has to be False as col 0 isn't integer
+  
+    # Concatenate all of these. This creates a very large file
+    # In order to make this faster, maybe just add values from other files into one file
+    all_stream_learning_data = pd.concat(dataframes)
+    all_stream_learning_data.to_csv(folder_file_prefix+"Cumulative.csv")
+  
+    all_stream_mean = {}
+    # average row by row
+    for i in range(num_rows): 
+      all_stream_mean[i] = all_stream_learning_data[i::num_rows].mean()
+      #print all_stream_mean[i]
+    all_stream_mean_df = pd.DataFrame(all_stream_mean)
+    all_stream_mean_df.to_csv(folder_file_prefix + "Mean.csv")
+    # Print result to file 
 
-  processes.append(subprocess.Popen(args, stdout=out1))
+#class SeededExperiment(Experiment):
+
+#  def __init__(self, seed):
+#    self.seed = seed
+
+class Experiment:
+  
+  # Take a command line and create a MOA process, which outputs results to a file.
+  def run(self, cmd, output_file, processes):
+  
+    args = shlex.split(cmd)
+    file_handle = output_file
+
+    #file_handle = os.path.join(MOA_DIR, output_file)
+    print(args)
+    print(file_handle) 
+  
+    
+    # create process
+    os.chdir(MOA_DIR)
+    out1 = open(file_handle, "w+")
+  
+    processes.append(subprocess.Popen(args, stdout=out1))
 
 if __name__=="__main__":
   main()
