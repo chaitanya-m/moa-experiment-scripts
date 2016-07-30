67b82bf19b7bbd3e37c6e23a1492765aebb72a1c
Chaitanya <chait.m@monash.edu>
2016-07-31 05:22:45 +1000

    Error is shown sequentially for the experiments... Have to combine into one diagram

--- experiments.py
+++ experiments.py
@@ -92,39 +92,36 @@ class ExperimentRunner:
     exit_codes = [p.wait() for p in processes]
     # wait until all experiments have finished running: each experiment corresponds to a process.
 
-  # Create individual MOA processes, write to CSV output files. Average over these.
-#  def average_over_streams(self, num_streams, output_folder, file_prefix):
-#  
-#  
-#    print output_files
-#  
-#    # Load the outputs into dataframes to prepare for averaging
-#  
-#    dataframes = []
-#    for file_name in output_files:
-#      print file_name
-#      dataframes.append(pd.read_csv(file_name, index_col=False, header=1, skiprows=0))
-#      # index_col has to be False as col 0 isn't integer
-#  
-#    # Concatenate all of these. This creates a very large file
-#    # In order to make this faster, maybe just add values from other files into one file
-#    all_stream_learning_data = pd.concat(dataframes)
-#    all_stream_learning_data.to_csv(folder_file_prefix + "Cumulative.csv")
-#
-#    all_stream_mean = {}
-#    # average row by row
-#    for i in range(num_rows): 
-#      all_stream_mean[i] = all_stream_learning_data[i::num_rows].mean()
-#
-#    #all_stream_mean_df = pd.DataFrame(all_stream_mean)
-#    all_stream_mean_df = pd.DataFrame(all_stream_mean).transpose() 
-#
-#    all_stream_mean_df.to_csv(folder_file_prefix + "Mean.csv")
-#    # Print result to file 
-#    
-#    all_stream_mean_df['error'] = (100.0 - all_stream_mean_df['classifications correct (percent)'])/100.0
-#
-#    Plot.plot_df(all_stream_mean_df)
+    output_files = prior_drift_mag_exp.getOutputFiles()
+
+    # Load the outputs into dataframes to prepare for averaging
+    for folder in output_files:
+      dataframes = []
+      for this_file in folder:
+        dataframes.append(pd.read_csv(this_file, index_col=False, header=1, skiprows=0))
+       # index_col has to be False as col 0 isn't integer
+
+      # Concatenate all of these. This creates a very large file
+      # In order to make this faster, maybe just add values from other files into one file
+      all_stream_learning_data = pd.concat(dataframes)
+      #all_stream_learning_data.to_csv(folder_file_prefix + "Cumulative.csv")
+  
+      all_stream_mean = {}
+      # average row by row
+      for i in range(num_rows): 
+        all_stream_mean[i] = all_stream_learning_data[i::num_rows].mean()
+  
+      #all_stream_mean_df = pd.DataFrame(all_stream_mean)
+      all_stream_mean_df = pd.DataFrame(all_stream_mean).transpose() 
+  
+      #all_stream_mean_df.to_csv(folder_file_prefix + "Mean.csv")
+      # Print result to file 
+      
+      all_stream_mean_df['error'] = (100.0 - all_stream_mean_df['classifications correct (percent)'])/100.0
+  
+      Plot.plot_df(all_stream_mean_df)
+
+   
 
 # A single MOA command creating a single MOA process
 class Experiment:
@@ -159,28 +156,36 @@ class ExperimentBuilder:
 
 class CompositeExperiment:
 
-  def __init__(self, exp_list):
+  def __init__(self, exp_list, output_files):
     self.exp_list = exp_list
+    self.output_files = output_files
   def getExperiments(self):
     return self.exp_list
+  def getOutputFiles(self):
+    return self.output_files
 
 class CompositeExperimentBuilder:
 
   @staticmethod
   def varyPriorDriftMagBuilder(num_streams, output_folder, file_prefix, processes):
     exp_list = []
+    output_files = []
     drift_mag_list = [0.1, 0.3, 0.5, 0.7]
     for drift_mag in drift_mag_list:
       this_output_folder = output_folder + '/' + str(drift_mag)
       folder_file_prefix = this_output_folder + '/' + file_prefix
       utilities.remove_folder(this_output_folder)
       utilities.make_folder(this_output_folder)
+      this_folder_output_files = []
 
       for i in range(0, num_streams):
         output_file = folder_file_prefix + str(i) + '.csv'
+        this_folder_output_files.append(output_file)
         exp_list.append(ExperimentBuilder.PriorDriftMagBuilder(drift_mag, output_file, processes))
 
-    return CompositeExperiment(exp_list)
+      output_files.append(this_folder_output_files)
+
+    return CompositeExperiment(exp_list, output_files)
 
 if __name__=="__main__":
 
