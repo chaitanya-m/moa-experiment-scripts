249c83345aad0d1ab4c892a84a7ad0da3317e655
Chaitanya Manapragada (Monash University) <cman39@monarch-login1.erc.monash.edu>
2019-07-31 21:45:16 +1000

    running with 52 learners

--- runexpSlurm.sh
+++ runexpSlurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH -N 10
+#SBATCH -N 15
 #SBATCH --job-name=moa_test
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=355G
-#SBATCH --ntasks-per-node=10
+#SBATCH --mem=640G
+#SBATCH --ntasks-per-node=4
 #SBATCH --mincpus=12
 #SBATCH --mem-per-cpu=3G
 
@@ -14,13 +14,14 @@
 #wait
 
 
+source myenv/bin/activate
+
 srun --ntasks=1 echo "hello" & 
 srun --ntasks=1 sleep 1 &
 srun --ntasks=1 echo "Running Experiments" &
 
-for i in {0..1}
+for i in {0..52}
 do
-srun --ntasks=1 time python2.7 ensembleexp.py $i
+srun --ntasks=1 time python2.7 ensembleexp.py $i 10
 wait
 done
-source myenv/bin/activate
