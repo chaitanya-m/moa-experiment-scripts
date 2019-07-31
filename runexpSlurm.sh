3a9928cc0c2569feab4e9a412ef58e5a9954dee6
Chaitanya Manapragada (Monash University) <cman39@monarch-login1.erc.monash.edu>
2019-07-31 22:53:35 +1000

    runexpSlurm.sh so it runs on multiple nodes

--- runexpSlurm.sh
+++ runexpSlurm.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#SBATCH -N 15
+#SBATCH --nodes=2
 #SBATCH --job-name=moa_test
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=640G
 #SBATCH --ntasks-per-node=4
 #SBATCH --mincpus=12
-#SBATCH --mem-per-cpu=3G
+#SBATCH --mem-per-cpu=5G
 
 #for i in `seq 100`; do  
 #srun --exclusive --nodes 1 --ntasks 1 ./program ${i} &
@@ -16,12 +15,8 @@
 
 source myenv/bin/activate
 
-srun --ntasks=1 echo "hello" & 
-srun --ntasks=1 sleep 1 &
-srun --ntasks=1 echo "Running Experiments" &
-
-for i in {0..52}
+for i in {0..51}
 do
-srun --ntasks=1 time python2.7 ensembleexp.py $i 10
-wait
+srun --nodes=1 time python2.7 ensembleexp.py $i 10 &
 done
+wait
