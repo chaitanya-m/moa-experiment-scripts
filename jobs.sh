9f9879039bdaba16d295b65c18281e029561c931
Chaitanya Manapragada (Monash University) <cman39@monarch-login1.erc.monash.edu>
2019-08-01 22:59:56 +1000

    slurm jobs per dataset

---
+++ jobs.sh
@@ -0,0 +1,24 @@
+sbatch --mem=96G --array [0-1039:20] runexpSlurm.sh
+
+sbatch --mem=32G --array [1-1039:20] runexpSlurm.sh
+
+sbatch --mem=16G --array [2-1039:20] runexpSlurm.sh
+sbatch --mem=16G --array [3-1039:20] runexpSlurm.sh
+sbatch --mem=16G --array [4-1039:20] runexpSlurm.sh
+sbatch --mem=16G --array [5-1039:20] runexpSlurm.sh
+
+sbatch --mem=8G --array [6-1039:20] runexpSlurm.sh
+sbatch --mem=8G --array [7-1039:20] runexpSlurm.sh
+sbatch --mem=8G --array [8-1039:20] runexpSlurm.sh
+sbatch --mem=8G --array [9-1039:20] runexpSlurm.sh
+sbatch --mem=8G --array [10-1039:20] runexpSlurm.sh
+sbatch --mem=4G --array [11-1039:20] runexpSlurm.sh
+sbatch --mem=4G --array [12-1039:20] runexpSlurm.sh
+sbatch --mem=4G --array [13-1039:20] runexpSlurm.sh
+sbatch --mem=4G --array [14-1039:20] runexpSlurm.sh
+sbatch --mem=4G --array [15-1039:20] runexpSlurm.sh
+sbatch --mem=4G --array [16-1039:20] runexpSlurm.sh
+sbatch --mem=4G --array [17-1039:20] runexpSlurm.sh
+
+sbatch --mem=2G --array [18-1039:20] runexpSlurm.sh
+sbatch --mem=2G --array [19-1039:20] runexpSlurm.sh
