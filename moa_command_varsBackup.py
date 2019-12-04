181fda0c430965f983b099189c2ddd621118238b
Chaitanya Manapragada (Monash University) <cman39@monarch-login1.erc.monash.edu>
2019-12-04 19:18:50 +1100

    Just before re-running ensemble results (ensembleexp has also become hatexp...) now after finishing up hat results, going back to efdt ensemble work. really should have separate files, or at least separate experiments

---
+++ moa_command_varsBackup.py
@@ -0,0 +1,28 @@
+
+#The parameter style in this file is different when the parameters correspond to MOAparameters directly (camel case vs underscores).
+
+PREFIX = "p2016030006"
+
+from os.path import expanduser
+HOME_DIR = expanduser("~")
+RESULTS_DIR = "{prefix}/exp_dir_results".format(prefix = PREFIX)
+DATA_DIR = "{prefix}/datasets".format(prefix = PREFIX)
+
+#HOME_DIR = '/home/chait'
+MOA_DIR = '{home_dir}/javacode/driftgen/dependency_jars'.format(home_dir = HOME_DIR)
+OUTPUT_DIR = '{resultsDir}/output'.format(resultsDir = RESULTS_DIR)
+FIG_DIR = '{resultsDir}/figures'.format(resultsDir = RESULTS_DIR)
+OUTPUT_PREFIX = 'out'
+
+#MOA_STUMP = "java -cp commons-math3-3.6.1.jar:guava-22.0.jar:moa.jar:cdgen3.jar -javaagent:sizeofag-1.0.0.jar"
+MOA_STUMP = "java -cp commons-math3-3.6.1.jar:guava-22.0.jar:moa.jar:cdgen3.jar -ea -Xmx2g"
+
+NUM_STREAMS = 10
+INDEX_COL = 'learning evaluation instances'
+FIELDS = """learning evaluation instances,evaluation time (cpu seconds),classified instances,classifications correct (percent),model training instances"""
+FIELDS_AVG = """classifications correct (percent)"""
+# broken with nonsensical values: model serialized size (bytes),model cost (RAM-Hours),model cost (RAM-Hours)
+# splits is per evaluation period, so it makes no sense in the end fields, only for fields_avg
+
+# java -cp commons-math3-3.6.1.jar:moa.jar:cdgen.jar -javaagent:sizeofag-1.0.0.jar moa.gui.GUI
+
